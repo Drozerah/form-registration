c015fbd3e063a44f411afd6705476d98832b47e8
Drozerah <drozerah@gmail.com>
2020-02-06 19:59:28 +0100

    feat: add underline effect utility

--- app.js
+++ app.js
@@ -17,9 +17,26 @@
   /**
   * Vibility Event 
   */
-  document.querySelector('#visibility')
-    .addEventListener('click', (e) => {
-      toogleVisibility(e.target)
+  document.querySelector('#visibility').addEventListener('click', e => toogleVisibility(e.target))
+  /**
+  * Inputs Underline Effect on Focus Events 
+  */
+  // Add/Remove class according event type
+  const focusEvents = (element, eventType) => {
+    switch (true) {
+      case eventType === 'focus':
+        element.classList.add('underlineFocus')
+        element.classList.remove('underline')
+      break
+      case eventType === 'focusout':
+        element.classList.add('underline')
+        element.classList.remove('underlineFocus')
+      break
     }
-  )
+  }
+  const underlinedElements = [...document.querySelectorAll('#resgistration_form .underline')]
+  underlinedElements.forEach(underline => {
+      underline.children[0].addEventListener('focus', (e) => focusEvents(e.target.parentElement, 'focus'))
+      underline.children[0].addEventListener('focusout',(e) => focusEvents(e.target.parentElement, 'focusout'))
+  })
 })()
